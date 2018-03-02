2f4aa8c44712707e4b74dc7f11d0711d12556b68
Alexander Wigmore <>
2018-03-02 11:24:40 +0000

    Tidy up of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,36 @@
 var gulp = require('gulp'),
-    cssnano = require('gulp-cssnano'),
-    autoprefixer = require('gulp-autoprefixer'),
-    connect = require('gulp-connect-php'),
-    sass = require('gulp-ruby-sass'),
-    browserSync = require('browser-sync');
+  cssnano = require('gulp-cssnano'),
+  autoprefixer = require('gulp-autoprefixer'),
+  connect = require('gulp-connect-php'),
+  sass = require('gulp-ruby-sass'),
+  browserSync = require('browser-sync');
 
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return sass('sass/style.scss', { style: 'compressed' })
     .pipe(autoprefixer())
-    // .pipe(cssnano())
+    .pipe(cssnano())
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 
 
-gulp.task('connect-sync', function() {
-  connect.server({}, function (){
+gulp.task('connect-sync', function () {
+  connect.server({}, function () {
     browserSync({
       proxy: '127.0.0.1:8000'
     });
-  });
-
-  gulp.watch('*.php').on('change', function () {
-    browserSync.reload();
   });
 });
 
 
 
 gulp.task('watch', function () {
-// gulp.task('watch', function () {
-    gulp.watch("sass/**", ['styles']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("sass/**", ['styles']);
+  gulp.watch(['js/**/*.js', '*.html', '*.php']).on('change', browserSync.reload);
 });
-gulp.task('default', ['styles', 'connect-sync'], function() {
-// gulp.task('default', ['styles'], function() {
+
+gulp.task('default', ['styles', 'connect-sync'], function () {
   gulp.start('watch');
 });
